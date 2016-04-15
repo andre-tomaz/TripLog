32394e661f441a00e067123e4a2b469f8963c0ba
Tommaz <aureliano_tomaz@outlook.com>
2016-04-15 18:57:15 -0300

    Added files via upload

---
+++ TripLog/NewEntryPage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace TripLog
+{
+	public class NewEntryPage : ContentPage
+	{
+		public NewEntryPage ()
+		{
+			Title = "New Entry";
+			var title = new EntryCell { Label = "Título" };
+			var latitude = new EntryCell { Label = "Latitude", Keyboard = Keyboard.Numeric };
+			var longitude = new EntryCell { Label = "Longitude", Keyboard = Keyboard.Numeric };
+			var date = new EntryCell { Label = "Date" };
+			var rating = new EntryCell { Label = "Rating", Keyboard = Keyboard.Numeric };
+			var notes = new EntryCell { Label = "Notes" };
+			var entryForm = new TableView {
+				Intent = TableIntent.Form,
+				Root = new TableRoot {
+					new TableSection () {
+						title,
+						latitude,
+						longitude,
+						date,
+						rating,
+						notes
+					}
+				}
+			};
+			var save = new ToolbarItem { Text = "save"};
+			ToolbarItems.Add (save);
+			Content = entryForm;
+		}
+	}
+}
+
+
